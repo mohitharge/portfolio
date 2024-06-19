801e8eae9703e4caf0b0bf91bbe166d03c400361
MOHIT HARGE <hargemohit@gmail.com>
2024-06-20 02:20:47 +0530

    next-sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://mohitharge.vercel.app', // Replace with your site URL
+  generateRobotsTxt: true, // Generate robots.txt file
+  // Additional options can be added here
+}
